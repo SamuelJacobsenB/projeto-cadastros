ebe88e1fc3facb3d6899f9eed20475eb28ec1b68
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-06-16 18:20:26 -0300

    Projeto quase terminado

---
+++ src/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+
+//---------------------------------
+
+const path = require('path');
+app.use(express.static(path.join(__dirname,'public')));
+
+//---------------------------------
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//---------------------------------
+
+const Handlebars = require('handlebars');
+const expressHandlebars = require('express-handlebars');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
+
+app.engine('handlebars', expressHandlebars.engine({
+    handlebars: allowInsecurePrototypeAccess(Handlebars),
+    defaultLayout: 'main',
+}));
+app.set('view engine', 'handlebars');
+
+//---------------------------------
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promisse;
+mongoose.connect('mongodb://localhost/cadastrosgithub').then(()=>{   
+    console.log('Conectado com sucesso ao MongoDB');
+}).catch(()=>{
+    console.log('Erro ao tentar se conectar ao MongoDB');
+});
+
+//---------------------------------
+
+const router = require('./routes/routes.js');
+
+//---------------------------------
+
+app.use('/main',router);
+
+//---------------------------------
+
+const PORT = 2024;
+app.listen(PORT,()=>{console.log('Server rodando...')});
---
+++ src/routes/routes.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const router = express.Router();
+
+//---------------------------------
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promisse;
+mongoose.connect('mongodb://localhost/cadastrosgithub');
+
+//---------------------------------;
+
+require('../models/Cadastros.js');
+const UserSchema = mongoose.model('cadastros');
+
+//---------------------------------
+
+router.get('/', (req, res)=>{  
+    UserSchema.find().then((cadastros)=>{
+        res.render('pages/index',{cadastros: cadastros});
+    }).catch((err)=>{
+        console.log(err);
+    });
+});
+
+router.get('/create',(req, res)=>{
+    res.render('pages/create');
+});
+
+router.get('/search',(req, res)=>{
+    res.render('pages/search');
+});
+
+router.post('/create/new',(req, res)=>{
+    var erros = [];
+
+    if(!req.body.name || req.body.name == null || req.body.name == undefined || req.body.name == ''){
+        erros.push({message: 'Nome inválido'});
+    };
+
+    if(!req.body.age || req.body.age == null || req.body.age == undefined || req.body.age == ''){
+        erros.push({message: 'Idade inválido'});
+    };
+
+    if(erros.length > 0){
+        res.redirect('/main/create');
+    } else {
+        const newUser = {
+            name: req.body.name,
+            age: req.body.age
+        };
+
+        new UserSchema(newUser).save()
+        .then(()=>{
+            res.redirect('/main');
+        })
+        .catch(()=>{
+            res.redirect('/main/create');
+        });
+    };
+});
+
+//---------------------------------
+
+module.exports = router;
