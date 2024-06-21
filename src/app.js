375764e83e2b25edf712f89b8c7491b2136db974
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-06-21 12:14:04 -0300

    Adicionando botão de edição

--- src/app.js
+++ src/app.js
@@ -36,6 +36,23 @@ mongoose.connect('mongodb://localhost/cadastrosgithub').then(()=>{
 
 //---------------------------------
 
+const session = require('express-session');
+const flash = require('connect-flash');
+app.use(session({
+    secret: 'samuel1234',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(flash());
+
+app.use((req,res,next)=>{
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+});
+
+//---------------------------------
+
 const router = require('./routes/routes.js');
 
 //---------------------------------
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -18,20 +18,13 @@ router.get('/', (req, res)=>{
     UserSchema.find().then((cadastros)=>{
         res.render('pages/index',{cadastros: cadastros});
     }).catch((err)=>{
-        console.log(err);
+        req.flash('error_msg','Erro ao listar cadastros');
+        res.redirect('/main');
     });
 });
 
 router.get('/create',(req, res)=>{
     res.render('pages/create');
-});
-
-router.get('/search',(req, res)=>{
-    UserSchema.find().then((cadastros)=>{
-        res.render('pages/search',{cadastros: cadastros});
-    }).catch((err)=>{
-        console.log(err);
-    });
 });
 
 router.post('/create/new',(req, res)=>{
@@ -46,7 +39,7 @@ router.post('/create/new',(req, res)=>{
     };
 
     if(erros.length > 0){
-        res.redirect('/main/create');
+        res.redirect('/main/create',{erros: erros});
     } else {
         const newUser = {
             name: req.body.name,
@@ -59,6 +52,56 @@ router.post('/create/new',(req, res)=>{
         })
         .catch(()=>{
             res.redirect('/main/create');
+        });
+    };
+});
+
+router.get('/search',(req, res)=>{
+    UserSchema.find().then((cadastros)=>{
+        res.render('pages/search',{cadastros: cadastros});
+    }).catch((err)=>{
+        console.log(err);
+    });
+});
+
+router.get('/edit/:id',(req,res)=>{
+    UserSchema.findOne({_id: req.params.id}).then((cadastro)=>{
+        res.render('pages/edit',{cadastro: cadastro});
+    }).catch((err)=>{
+        req.flash('error_msg','Usuário não encontrado');
+        res.redirect('/main/search');
+    });
+});
+
+router.post('/edit',(req,res)=>{
+    var erros = [];
+
+    if(!req.body.name || req.body.name == null || req.body.name == undefined || req.body.name == ''){
+        erros.push({message: 'Nome inválido'});
+    };
+
+    if(!req.body.age || req.body.age == null || req.body.age == undefined || req.body.age == ''){
+        erros.push({message: 'Idade inválido'});
+    };
+
+    if(erros.length > 0){
+        res.redirect('/main/search',{erros: erros});
+    } else {
+        UserSchema.findOne({_id: req.body.id}).then((cadastro)=>{
+
+            cadastro.name = req.body.name;
+            cadastro.age = req.body.age;
+
+            cadastro.save().then(()=>{
+                req.flash('success_msg','Cadastro editado com sucesso');
+                res.redirect('/main/search');
+            }).catch((err)=>{
+                req.flash('error_msg','Houve um erro interno ao salvar edição');
+                res.redirect('/main/search');
+            });
+
+        }).catch((err)=>{
+            req.flash('error_msg','Usuário não encontrado');
         });
     };
 });
